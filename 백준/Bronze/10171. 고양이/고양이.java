ab179b9268719942812421ef1989c80327bcf371
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-08 03:54:07 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11200 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,14 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("\\    /\\").append("\n");
+		sb.append(" )  ( ')").append("\n");
+		sb.append("(  /  )").append("\n");
+		sb.append(" \\(__)|").append("\n");
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
