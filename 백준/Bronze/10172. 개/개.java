20478bf2103ae56d8c59e76ddce209771ed36d1e
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-08 04:42:13 +0900

    [Bronze V] Title: 개, Time: 76 ms, Memory: 11364 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+public class Main {
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("|\\_/|").append("\n");
+		sb.append("|q p|   /}").append("\n");
+		sb.append("( 0 )\"\"\"\\").append("\n");
+		sb.append("|\"^\"`    |").append("\n");
+		sb.append("||_/=\\\\__|").append("\n");
+		
+		System.out.println(sb.toString());
+	}
+
+}
