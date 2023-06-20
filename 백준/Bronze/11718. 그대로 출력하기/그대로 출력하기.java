c7a34872c168192d88d70234e55d88d0405e131f
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-20 18:59:36 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 80 ms, Memory: 11684 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -4,22 +4,22 @@ import java.io.InputStreamReader;
 import java.util.NoSuchElementException;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
-		while(true) {
-			try {
-				String word = br.readLine();
-				if(word.isEmpty()) {
+		String word = "";
+
+		try {
+			while ((word = br.readLine()) != null || !word.isEmpty()) {
+				if (word.isEmpty()) // .size는 시간 복잡도가 N인데 isEmpty는 시간 복잡도가 1이다.
 					break;
-				}
 				sb.append(word).append("\n");
-			}catch(NullPointerException | NoSuchElementException e){
-				break;
+
 			}
+		} catch (NullPointerException | NoSuchElementException e) {
+			
 		}
 		System.out.print(sb.toString());
 	}
