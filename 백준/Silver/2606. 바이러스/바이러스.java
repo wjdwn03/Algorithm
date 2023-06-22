dc3a1fcf264a48752a5f835b84acd54b50884c52
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-22 21:53:20 +0900

    [Silver III] Title: 바이러스, Time: 80 ms, Memory: 11620 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,61 +1,67 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	static int computer, twin, cnt;
-	static List<Integer>[] list;
-	static boolean[] visit;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		computer = Integer.parseInt(br.readLine());
-		twin = Integer.parseInt(br.readLine());
-		visit = new boolean[computer + 1];
-		cnt = 0;
-		list = new ArrayList[computer + 1];
-
-		StringTokenizer st;
-
-		// 리스트 초기화
-		for (int i = 0; i <= computer; i++) {
-			list[i] = new ArrayList<>();
+	
+	static class Node{
+		
+		int to;
+		Node next;
+		
+		public Node(int to, Node next) {
+			this.to = to;
+			this.next = next;
 		}
-
-		int from = 0, to = 0;
-
-		for (int i = 0; i < twin; i++) {
+	}
+	
+	static int computer, edge, cnt;
+	static Node[] node;
+	static boolean[] visit;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		computer = Integer.parseInt(br.readLine());
+		edge = Integer.parseInt(br.readLine());
+		
+		cnt = 0;
+		node = new Node[computer+1];
+		visit = new boolean[computer+1];
+		
+		StringTokenizer st;
+		
+		int from=0, to=0;
+		
+		for(int i=0; i<edge; i++) {
 			st = new StringTokenizer(br.readLine());
 			from = Integer.parseInt(st.nextToken());
 			to = Integer.parseInt(st.nextToken());
-			list[from].add(to);
-			list[to].add(from);
-		} // end input
-
+			
+			// 꼬리에 넣는게 아니라 헤더쪽에 넣어준다.
+			node[from] = new Node(to, node[from]);
+			node[to] = new Node(from, node[to]);
+		}
+		
 		dfs(1);
-
 		System.out.println(cnt);
-
+		
 	}
 
 	private static void dfs(int cur) {
-
+		
 		visit[cur] = true;
-
-		for (int i : list[cur]) {
-			
-			if (!visit[i]) {
-				dfs(i);
+		
+		// node가 끝까지 다 돌 때까지 반복
+		for(Node temp=node[cur]; temp!=null; temp=temp.next) {
+			if(!visit[temp.to]) {
 				cnt++;
+				dfs(temp.to);
 			}
-			
 		}
-
+		
 	}
 
 }
