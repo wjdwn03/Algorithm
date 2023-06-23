ad403a23d6324fdf7616be05b0df5e3fce2f9e91
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-24 02:48:46 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	static int N;
+	static int[] stair;
+	static int[] max;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		stair = new int[301];
+		max = new int[301];
+
+		for (int i = 1; i <= N; i++) {
+			stair[i] = Integer.parseInt(br.readLine());
+		}
+
+		max[1] = stair[1];
+		max[2] = stair[1] + stair[2];
+		max[3] = stair[3] + Math.max(stair[1], stair[2]);
+
+		for (int i = 4; i <= N; i++) {
+			max[i] = stair[i] + Math.max(max[i - 3] + stair[i - 1], max[i - 2]);
+		}
+
+		System.out.println(max[N]);
+	}
+
+}
