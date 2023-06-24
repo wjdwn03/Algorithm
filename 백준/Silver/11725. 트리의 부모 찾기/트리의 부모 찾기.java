0e9f1f81cdc6d1620ed67c448924360b25e9974a
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-06-24 12:25:32 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 548 ms, Memory: 68980 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -8,66 +8,55 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
+
 	static int N;
 	static List<Integer>[] list;
 	static int[] arr;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
-		
+
 		N = Integer.parseInt(br.readLine());
-		list = new ArrayList[N+1];
-		arr = new int[N+1];
-		
-		for(int i=0; i<=N; i++) {
+		list = new ArrayList[N + 1];
+		arr = new int[N + 1];
+		boolean[] visit = new boolean[N + 1];
+
+		for (int i = 0; i <= N; i++) {
 			list[i] = new ArrayList<>();
 		}
-		
-		
-		for(int i=1; i<N; i++) {
+
+		for (int i = 1; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			
+
 			list[a].add(b);
 			list[b].add(a);
 		}
-		
-		bfs(1);
-		
-		for(int i=2; i<=N; i++) {
+		visit[1] = true;
+		dfs(1, visit);
+
+		for (int i = 2; i <= N; i++) {
 			sb.append(arr[i]).append("\n");
 		}
 		System.out.println(sb.toString());
-		
+
 	}
 
-	private static void bfs(int idx) {
+	private static void dfs(int idx, boolean[] visit) {
 
-		Queue<Integer> queue = new ArrayDeque<>();
-		boolean[] visit = new boolean[N+1];
-		
-		visit[idx] = true;
-		queue.offer(idx);
-		
-		while(!queue.isEmpty()) {
-			int cur = queue.poll();
-			
-			for(int i=0; i<list[cur].size(); i++) {
-				int now = list[cur].get(i);
-				
-				if(!visit[now]) {
-					visit[now] = true;
-					queue.offer(now);
-					arr[now] = cur;
-				}
+		for (int i = 0; i < list[idx].size(); i++) {
+			int now = list[idx].get(i);
+
+			if (!visit[now]) {
+				visit[now] = true;
+				arr[now] = idx;
+				dfs(now, visit);
 			}
-			
 		}
-		
+
 	}
 
 }
