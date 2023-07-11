26cd7fbb6fa75631665dd2a716d9cdc993641ca2
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-11 15:15:40 +0900

    [Silver IV] Title: 접미사 배열, Time: 108 ms, Memory: 17540 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		
+
 		String S = br.readLine();
 		int size = S.length();
-		
-		Queue<String> queue = new PriorityQueue<>();
-		
-		for(int i=0; i<size; i++) {
-//			String word = "";
-//			for(int j=size-i; j<size; j++) {
-//				word+=S.charAt(j);
-//			}
-			queue.add(S.substring(i));
+		String[] arr = new String[size];
+
+		for (int i = 0; i < size; i++) {
+			arr[i] = S.substring(i);
 		}
-		
-		while(!queue.isEmpty()) {
-			sb.append(queue.poll()).append("\n");
+
+		Arrays.sort(arr);
+
+		for (int i = 0; i < size; i++) {
+			sb.append(arr[i]).append("\n");
 		}
-		
+
 		System.out.println(sb.toString());
-		
+
 	}
 
 }
