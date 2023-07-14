31ab11f89b62657a6db4d0f84b37cfd7723bfe31
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-14 16:33:55 +0900

    [Silver IV] Title: 로프, Time: 244 ms, Memory: 21056 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,21 +12,20 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		List<Integer> list = new ArrayList<>();
+		int[] arr = new int[N];
 		
 		for(int n = 0; n<N; n++) {
-			list.add(Integer.parseInt(br.readLine()));
+			arr[n] = Integer.parseInt(br.readLine());
 		}
 		
 		// 오름차순 정렬
-		Collections.sort(list);
+		Arrays.sort(arr);
 		
-		
-		int max = list.get(N-1); // 정답
+		int max = arr[N-1]; // 정답
 		
 		for(int n=N-1; n>=0; n--) {
 			
-			int w = list.get(n) * (N-n);
+			int w = arr[n] * (N-n);
 			
 			if(max >= w) // (최소 중량 * n) 이 max의 값보다 작거나 같으면 넘긴다.
 				continue;
