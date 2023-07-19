1b7eec59c8484d58755644de3da8966e35ef7178
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-19 23:43:43 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -14,44 +14,26 @@ public class Main {
 		int K = Integer.parseInt(st.nextToken());
 		int[] coins = new int[N];
 		int cnt = 0; // 정답
-		int maxIdx = -1; // K보다 작은 동전의 가치 중 가장 큰 동전의 index
 		
 		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(br.readLine());
 			
-			// 크다면 더 이상 볼 필요 없다.
-			if(num>K) {
-				maxIdx = i-1;
-				break;
-			}
-			// 같은 경우 그 동전 한 개만 쓰면 된다.
-			if(num==K) {
-				cnt = 1;
-				break;
-			}
-			
-			coins[i] = num;
+			coins[i] = Integer.parseInt(br.readLine());
 			
 		} // end input
 		
 		
-		if(cnt!=1) {
+		for(int i = N-1; i>=0; i--) {
 			
-			// "가장 큰 동전의 가치 < K" 인 경우 N-1 위치부터 계산해봐야 한다.
-			if(maxIdx==-1) {
-				maxIdx=N-1;
-			}
-			
-			for(int i = maxIdx; i>=0; i--) {
+			if(coins[i] <= K) {
 				
 				cnt += K/coins[i];
 				K %= coins[i];
-				
-				if(K==0) {
-					break;
-				}
 			}
-		} 
+			
+			if(K==0) {
+				break;
+			}
+		}
 		
 		System.out.println(cnt);
 	}
