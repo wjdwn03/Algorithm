bfe8b5755bd38b8e98e86bb9ffcb0006bc8246de
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-20 17:05:57 +0900

    [Silver III] Title: 에너지 드링크, Time: 352 ms, Memory: 34780 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] arr = new int[N];
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(arr);
+
+		double energy = arr[N - 1];
+
+		for (int i = N - 2; i >= 0; i--) {
+			energy += arr[i] / 2.0;
+		}
+
+		System.out.println(energy);
+	}
+
+}
