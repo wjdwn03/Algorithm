3e657e771736c8ff53d3f1b5999e8a98af2b9503
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-20 12:03:33 +0900

    refactor: modify to correct code

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -10,35 +10,27 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int[] road = new int[N - 1]; // 도로 길이 저장
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		// 도로 길이 입력
-		for (int i = 0; i < N - 1; i++) {
-			road[i] = Integer.parseInt(st.nextToken());
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine()); // 도로 길이
+		StringTokenizer st2 = new StringTokenizer(br.readLine()); // 리터당 가격
 
-		st = new StringTokenizer(br.readLine());
-		
 		// 리터당 최소 가격
-		int min = Integer.MAX_VALUE;
+		long min = 1000000001;
 		// 정답
-		int ans = 0; 
-		
+		long ans = 0;
+
 		// 제일 마지막에 있는 도시의 가격은 필요 없기 때문에 N-2까지만 본다.
 		for (int i = 0; i < N - 1; i++) {
-			
-			
-			int gas = Integer.parseInt(st.nextToken());
-			
+
+			int gas = Integer.parseInt(st2.nextToken());
+			int road = Integer.parseInt(st.nextToken());
+
 			if (gas < min) {
-				min = gas;
+				min = (long) gas;
 			}
 
-			ans += road[i] * min; // 모든 도시에 갈 때마다 최소 가격으로만 구매
+			ans += road * min; // 모든 도시에 갈 때마다 최소 가격으로만 구매
 		}
-		
+
 		System.out.println(ans);
 	}
 
