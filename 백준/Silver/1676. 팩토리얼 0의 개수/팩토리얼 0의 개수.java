b82a478edce90201b50169382c90479976ce1064
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-21 12:51:14 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 84 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -10,24 +10,7 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		// N이 500까지 가능하므로 long의 범위를 넘어서기 때문에 무한대의 범위를 가진 BigInteger를 사용
-		BigInteger factorial = BigInteger.ONE; // 1
-		
-		for(int i=N; i>0; i--) {
-			factorial = factorial.multiply(BigInteger.valueOf(i)); // 곱셈
-		}
-		
-		String num = factorial.toString();
-		int cnt = 0;
-		
-		for(int i=num.length()-1; i>=0; i--) {
-			if(num.charAt(i)=='0') {
-				cnt++;
-			}
-			else {
-				break;
-			}
-		}
+		int cnt = N/5 + N/25 + N/125;
 		
 		System.out.println(cnt);
 		
