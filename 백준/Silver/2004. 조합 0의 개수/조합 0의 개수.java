90cb46f6afcc8420616654f016d97862562ec21d
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-21 17:24:12 +0900

    [Silver II] Title: 조합 0의 개수, Time: 80 ms, Memory: 11552 KB -BaekjoonHub

---
+++ 백준/Silver/2004. 조합 0의 개수/조합 0의 개수.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long n = Long.parseLong(st.nextToken());
+		long m = Long.parseLong(st.nextToken());
+
+		long five = countZero(n, true) - countZero(n - m, true) - countZero(m, true);
+		long two = countZero(n, false) - countZero(n - m, false) - countZero(m, false);
+
+		// 팩토리얼 0의 개수는 하나의 숫자에 대한 팩토리얼을 구하는 것이기 때문에 무조건 2가 포함되어 있다.
+		// 그래서 5로 나는 것만 구해도 괜찮다.
+		// 조합은 공식으로 인해 2가 포함되지 않는 경우도 있기 때문에 5와 2 둘 다 나눠서 적은 것을 구해야 한다. ex) 5C1
+		long ans = Math.min(five, two);
+
+		System.out.println(ans);
+	}
+
+	public static long countZero(long num, boolean isFive) {
+
+		long divideNum = 2;
+
+		if (isFive)
+			divideNum = 5;
+
+		long cnt = 0;
+
+		while (num >= divideNum) {
+			cnt += num / divideNum;
+
+			num /= divideNum;
+		}
+
+		return cnt;
+
+	}
+
+}
