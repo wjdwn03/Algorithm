27e26b3050da67e3bb0f810750b5492f47607eec
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-07-27 16:10:55 +0900

    [Silver I] Title: 회의실 배정, Time: 456 ms, Memory: 41740 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -3,17 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
 		int N = Integer.parseInt(br.readLine());
 
-		Meeting[] arr = new Meeting[N];
+		Queue<Meeting> pq = new PriorityQueue<>();
 
 		for (int i = 0; i < N; i++) {
 
@@ -22,28 +24,26 @@ public class Main {
 			int start = Integer.parseInt(st.nextToken());
 			int end = Integer.parseInt(st.nextToken());
 
-			arr[i] = new Meeting(start, end);
+			pq.add(new Meeting(start, end));
 
 		} // end input
 
-		// end 시간을 기준으로 하되 end가 같으면 start를 기준으로 정렬
-		Arrays.sort(arr);
-
-		Meeting before = arr[0]; // 회의실 사용하고 있는 회의
+		Meeting before = pq.poll(); // 회의실 사용하고 있는 회의
 
 		int cnt = 1; // 정답
 
 		for (int i = 1; i < N; i++) {
 
-			// arr[i]에 있는 회의 시간에 회의실이 비어있는 경우
-			if (before.end <= arr[i].start) {
-				before = arr[i];
+			Meeting now = pq.poll();
+
+			// now에 있는 회의 시간에 회의실이 비어있는 경우
+			if (before.end <= now.start) {
+				before = now;
 				cnt++;
 			}
 		}
 
 		System.out.println(cnt);
-
 	}
 
 	static class Meeting implements Comparable<Meeting> {
