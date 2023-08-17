66dd8da1752e50b444fa7887d7d258c5e4255c26
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-08-17 15:55:32 +0900

    [Silver II] Title: 이동하기, Time: 436 ms, Memory: 76896 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static int[][] arr;
+	static int[][] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		arr = new int[N + 1][M + 1];
+		dp = new int[N + 1][M + 1];
+
+		for (int i = 1; i <= N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= M; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				dp[i][j] = arr[i][j];
+			}
+
+		} // end input
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= M; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j] + arr[i][j], dp[i][j - 1] + arr[i][j]);
+			}
+		}
+
+		System.out.println(dp[N][M]);
+
+	}
+
+}
