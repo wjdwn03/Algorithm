464b8c941c5f6e8ce9d605b164010bfc3e66d414
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-09-11 00:33:59 +0900

    [Bronze II] Title: 진법 변환, Time: 76 ms, Memory: 11544 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -7,27 +7,30 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		String N = st.nextToken();
+
 		int B = Integer.parseInt(st.nextToken());
-		int ans = 0;
 
-		for (int i = 0; i < N.length(); i++) {
+		int size = N.length();
+		int sum = 0;
 
-			int num = 0;
+		int idx = 0;
 
-			if (N.charAt(i) >= '0' && N.charAt(i) <= '9') {
-				num = N.charAt(i) - '0';
-			} else {
-				num = N.charAt(i) - 'A' + 10;
+		// N의 1의 자리부터 계산한다.
+		for (int i = size - 1; i >= 0; i--) {
+			int now = N.charAt(i) - '0';
+
+			if (now > 9) {
+				now -= 7; // 문제에서 A는 10인데 'A' - '0'을 해보면 17이 나온다. 그래서 7을 빼줌.
 			}
 
-			ans += num * Math.pow(B, N.length() - i - 1);
-		} // end for
+			sum += (int) Math.pow(B, idx++) * now;
 
-		System.out.println(ans);
+		}
+
+		System.out.println(sum);
 	}
 
 }
