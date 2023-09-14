4d3e6d71d713f3482e5f8eaf0af4a7ec71149167
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-09-14 22:06:13 +0900

    [Silver II] Title: 아기 상어 2, Time: 88 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -9,6 +10,7 @@ public class Main {
 
 	static int N, M, ans;
 	static int[][] arr;
+	static boolean[][] visit;
 	static int[] di = { -1, 1, 0, 0, -1, 1, 1, -1 }; // 상,하,좌,우,왼위,왼하,우하,우상
 	static int[] dj = { 0, 0, -1, 1, -1, -1, 1, 1 };
 
@@ -19,78 +21,65 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
+		visit = new boolean[N][M];
 
 		ans = 0;
+
+		Queue<Point> queue = new ArrayDeque<>();
 
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < M; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
+				if (arr[i][j] == 1) {
+					queue.offer(new Point(i, j));
+					visit[i][j] = true;
+				}
 			}
 		} // end input
 
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (arr[i][j] == 0) {
-					bfs(i, j);
-				}
-			}
-		}
+		bfs(queue);
 
 		System.out.println(ans);
 	}
 
-	private static void bfs(int starti, int startj) {
+	private static void bfs(Queue<Point> queue) {
 
-		Queue<Point> queue = new PriorityQueue<>();
-		boolean[][] visit = new boolean[N][M];
-
-		queue.offer(new Point(starti, startj, 0));
-		visit[starti][startj] = true;
-
-		int min = Integer.MAX_VALUE;
+		int cnt = -1;
 
 		while (!queue.isEmpty()) {
-			Point cur = queue.poll();
 
-			if (arr[cur.i][cur.j] == 1) {
-				min = Math.min(min, cur.dis);
-				continue;
+			int size = queue.size();
+
+			for (int s = 0; s < size; s++) {
+				Point cur = queue.poll();
+
+				for (int d = 0; d < 8; d++) {
+					int nexti = cur.i + di[d];
+					int nextj = cur.j + dj[d];
+
+					if (nexti < 0 || nexti >= N || nextj < 0 || nextj >= M || visit[nexti][nextj])
+						continue;
+
+					visit[nexti][nextj] = true;
+					queue.offer(new Point(nexti, nextj));
+				}
 			}
-
-			for (int d = 0; d < 8; d++) {
-				int nexti = cur.i + di[d];
-				int nextj = cur.j + dj[d];
-
-				if (nexti < 0 || nexti >= N || nextj < 0 || nextj >= M || visit[nexti][nextj])
-					continue;
-
-				int distance = cur.dis + 1;
-
-				if (distance > min)
-					continue;
-
-				visit[nexti][nextj] = true;
-				queue.offer(new Point(nexti, nextj, distance));
-			}
+			cnt++;
 		} // end while
 
-		ans = Math.max(ans, min);
+		ans = cnt;
+
 	}
 
-	static class Point implements Comparable<Point> {
-		int i, j, dis;
+	static class Point {
+		int i, j;
 
-		public Point(int i, int j, int dis) {
+		public Point(int i, int j) {
 			this.i = i;
 			this.j = j;
-			this.dis = dis;
 		}
 
-		@Override
-		public int compareTo(Point o) {
-			return this.dis - o.dis;
-		}
 	}
 
 }
