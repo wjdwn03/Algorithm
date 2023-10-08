be86c38239c365661e9ccc7d320cf229897cab6c
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-10-08 12:09:39 +0900

    [Bronze V] Title: N 찍기, Time: 164 ms, Memory: 17132 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			sb.append(i).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
