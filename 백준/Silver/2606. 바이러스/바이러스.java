12cebd0f96000a724ed9a656c7f42ffdf4e0d09b
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-10-11 12:05:03 +0900

    [Silver III] Title: 바이러스, Time: 76 ms, Memory: 11728 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,67 +1,88 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static class Node{
-		
-		int to;
-		Node next;
-		
-		public Node(int to, Node next) {
-			this.to = to;
-			this.next = next;
-		}
-	}
-	
-	static int computer, edge, cnt;
-	static Node[] node;
-	static boolean[] visit;
-	
+
+	static int N, M;
+	static List<Integer>[] list;
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		computer = Integer.parseInt(br.readLine());
-		edge = Integer.parseInt(br.readLine());
-		
-		cnt = 0;
-		node = new Node[computer+1];
-		visit = new boolean[computer+1];
-		
 		StringTokenizer st;
-		
-		int from=0, to=0;
-		
-		for(int i=0; i<edge; i++) {
+
+		N = Integer.parseInt(br.readLine());
+
+		list = new ArrayList[N + 1];
+
+		// initialization
+		for (int i = 0; i <= N; i++) {
+			list[i] = new ArrayList<>();
+		}
+
+		M = Integer.parseInt(br.readLine());
+
+		int from = 0, to = 0;
+
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
+
 			from = Integer.parseInt(st.nextToken());
 			to = Integer.parseInt(st.nextToken());
-			
-			// 꼬리에 넣는게 아니라 헤더쪽에 넣어준다.
-			node[from] = new Node(to, node[from]);
-			node[to] = new Node(from, node[to]);
-		}
-		
-		dfs(1);
+
+			list[from].add(to);
+			list[to].add(from);
+		} // end input
+
+		int cnt = bfs();
+
 		System.out.println(cnt);
-		
+
 	}
 
-	private static void dfs(int cur) {
-		
-		visit[cur] = true;
-		
-		// node가 끝까지 다 돌 때까지 반복
-		for(Node temp=node[cur]; temp!=null; temp=temp.next) {
-			if(!visit[temp.to]) {
-				cnt++;
-				dfs(temp.to);
-			}
+	private static int bfs() {
+
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean[] visit = new boolean[N + 1];
+
+		int size = list[1].size(); // 한 컴퓨터에 연결된 컴퓨터 개수
+		int now = 0;
+		int cnt = size;
+
+		visit[1] = true;
+
+		// 1번 컴퓨터에 연결된 컴퓨터 방문 체크 & 큐에 담기
+		for (int i = 0; i < size; i++) {
+			now = list[1].get(i);
+			queue.offer(now);
+			visit[now] = true;
 		}
-		
-	}
+
+		while (!queue.isEmpty()) {
+			int cur = queue.poll(); // 현재 컴퓨터 번호
+
+			size = list[cur].size();
+
+			// 연결된 모든 컴퓨터 중 방문하지 않은 컴퓨터를 방문처리
+			for (int i = 0; i < size; i++) {
+				now = list[cur].get(i);
+
+				if (visit[now])
+					continue;
+
+				queue.offer(now);
+				visit[now] = true;
+				cnt++;
+
+			}
+		} // end while
+		return cnt;
+
+	} // end bfs
 
 }
