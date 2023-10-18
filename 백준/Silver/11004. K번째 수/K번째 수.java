4ab760b992f1254b83ee36d52e48fad11ae4214f
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-10-18 23:03:17 +0900

    [Silver V] Title: K번째 수, Time: 4648 ms, Memory: 663872 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			pq.add(Integer.parseInt(st.nextToken()));
+		}
+
+		for (int i = 0; i < K - 1; i++) {
+			pq.poll();
+		}
+		System.out.println(pq.poll());
+
+	}
+
+}
