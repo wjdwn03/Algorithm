d090a6576686f43e271d544fc2d113d9c66ef7af
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-11-18 23:51:53 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 72 ms, Memory: 11384 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("    8888888888  888    88888").append("\n");
+		sb.append("   88     88   88 88   88  88").append("\n");
+		sb.append("    8888  88  88   88  88888").append("\n");
+		sb.append("       88 88 888888888 88   88").append("\n");
+		sb.append("88888888  88 88     88 88    888888").append("\n");
+		sb.append("\n");
+		sb.append("88  88  88   888    88888    888888").append("\n");
+		sb.append("88  88  88  88 88   88  88  88").append("\n");
+		sb.append("88 8888 88 88   88  88888    8888").append("\n");
+		sb.append(" 888  888 888888888 88  88      88").append("\n");
+		sb.append("  88  88  88     88 88   88888888");
+
+		System.out.println(sb.toString());
+	}
+
+}
