e3e53c5615aca38fe518074d53d9a2c62f9879d7
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-12-01 23:54:48 +0900

    [Bronze V] Title: 콜센터, Time: 72 ms, Memory: 11212 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,20 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("     /~\\").append("\n");
+		sb.append("    ( oo|").append("\n");
+		sb.append("    _\\=/_").append("\n");
+		sb.append("   /  _  \\").append("\n");
+		sb.append("  //|/.\\|\\\\").append("\n");
+		sb.append(" ||  \\ /  ||").append("\n");
+		sb.append("============").append("\n");
+		sb.append("|          |").append("\n");
+		sb.append("|          |").append("\n");
+		sb.append("|          |").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
