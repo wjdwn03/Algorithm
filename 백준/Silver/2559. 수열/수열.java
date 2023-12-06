663fdc181fb3a3a58e071be3d16c26ea9c811f00
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-12-06 18:21:00 +0900

    [Silver III] Title: 수열, Time: 280 ms, Memory: 23484 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+
+			// 누적합
+			arr[i] += arr[i - 1];
+		}
+
+		int max = Integer.MIN_VALUE;
+
+		for (int i = K; i <= N; i++) {
+			max = Math.max(max, arr[i] - arr[i - K]);
+		}
+
+		System.out.println(max);
+	}
+
+}
