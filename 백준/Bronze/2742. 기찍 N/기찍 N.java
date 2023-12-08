2875dfd628f691a1ab57637b1e208c525980d10d
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-12-08 23:45:44 +0900

    [Bronze IV] Title: 기찍 N, Time: 160 ms, Memory: 17224 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int n = N; n > 0; n--) {
+			sb.append(n).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+}
