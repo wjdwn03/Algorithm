2103574757d3c3558491c84a0494eff5dd8e6a8f
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2023-12-14 23:57:42 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 76 ms, Memory: 11196 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE").append("\n");
+		sb.append("N2 Bomber      Heavy Fighter  Limited    21        ").append("\n");
+		sb.append("J-Type 327     Light Combat   Unlimited  1         ").append("\n");
+		sb.append("NX Cruiser     Medium Fighter Limited    18        ").append("\n");
+		sb.append("N1 Starfighter Medium Fighter Unlimited  25        ").append("\n");
+		sb.append("Royal Cruiser  Light Combat   Limited    4         ").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
