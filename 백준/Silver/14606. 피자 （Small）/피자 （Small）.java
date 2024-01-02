4b7dbfc5020fd12f82fe6f250a37cb64d13c21c8
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2024-01-03 01:02:32 +0900

    [Silver V] Title: 피자 (Small), Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/14606. 피자 （Small）/피자 （Small）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int N;
+	static int[] dp;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		dp = new int[N + 1];
+
+		int ans = dfs(N);
+
+		System.out.println(ans);
+	}
+
+	private static int dfs(int n) {
+
+		if (n <= 1)
+			return 0;
+
+		if (dp[n] == 0) {
+
+			dp[n] = n / 2 * (n - n / 2) + dfs(n / 2) + dfs(n - n / 2);
+		}
+
+		return dp[n];
+
+	}
+
+}
