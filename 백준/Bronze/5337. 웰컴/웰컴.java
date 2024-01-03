b09aa7c4f8f6f63591480b28724f745e4430a7fe
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2024-01-03 14:51:41 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(".  .   .").append("\n");
+		sb.append("|  | _ | _. _ ._ _  _").append("\n");
+		sb.append("|/\\|(/.|(_.(_)[ | )(/.").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
