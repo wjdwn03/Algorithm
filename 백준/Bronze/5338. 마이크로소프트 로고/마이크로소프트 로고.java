1b04c3c8a4b5afa1ebca9c4fe99d65ae6aed673f
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2024-01-06 23:58:13 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("       _.-;;-._").append("\n");
+		sb.append("'-..-'|   ||   |").append("\n");
+		sb.append("'-..-'|_.-;;-._|").append("\n");
+		sb.append("'-..-'|   ||   |").append("\n");
+		sb.append("'-..-'|_.-''-._|").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
