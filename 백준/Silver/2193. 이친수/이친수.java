9825cd70c293c471137cf142179f96972b7817f8
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2024-01-08 20:47:58 +0900

    [Silver III] Title: 이친수, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[N + 1];
+
+		dp[1] = 1L;
+
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+
+		System.out.println(dp[N]);
+
+	}
+
+}
