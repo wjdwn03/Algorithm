9805ba54e10d08dccdff9674d8ee4313b50da84d
wjdwn03 <109848753+wjdwn03@users.noreply.github.com>
2024-01-25 23:22:44 +0900

    [Bronze IV] Title: 입실 관리, Time: 124 ms, Memory: 14164 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			sb.append(br.readLine().toLowerCase()).append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
