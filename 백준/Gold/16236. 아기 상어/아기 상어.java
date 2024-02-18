dc2d1d7ac0231af69e436cac80c8e1c9c244be23
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-02-18 23:50:03 +0900

    [Gold III] Title: 아기 상어, Time: 132 ms, Memory: 14672 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -12,8 +12,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N, baby, cnt, time;
-	static List<Point> list;
+	static int N, baby, cnt, time, fishCnt, sharkI, sharkJ;
 	static int[][] arr;
 	static int[] di = { -1, 1, 0, 0 };
 	static int[] dj = { 0, 0, -1, 1 };
@@ -27,108 +26,61 @@ public class Main {
 		baby = 2; // 아기 상어 크기
 		cnt = 0; // 아기 상어가 먹은 물고기 수
 		time = 0; // 이동 시간
+		fishCnt = 0; // 물고기 수
 
-		list = new LinkedList<>();
 		arr = new int[N][N];
 
-		int starti = 0, startj = 0; // 아기 상어의 시작 위치
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 
 				if (arr[i][j] == 9) {
-					starti = i;
-					startj = j;
+					sharkI = i;
+					sharkJ = j;
 					arr[i][j] = 0;
 				}
 
 				else if (arr[i][j] > 0 && arr[i][j] < 7) {
-					list.add(new Point(i, j, arr[i][j]));
+					fishCnt++;
 				}
 			}
 		} // end input
 
-		// 아기 상어보다 작은 물고기를 쉽게 찾기 위해 정렬
-		Collections.sort(list, (o1, o2) -> o1.weight - o2.weight);
-
-		move(starti, startj);
+		bfs();
 
 		System.out.println(time);
 	}
 
-	private static void move(int starti, int startj) {
+	private static void bfs() {
 
-		Queue<Point> queue = new ArrayDeque<>();
+		PriorityQueue<Point> pq = new PriorityQueue<>();
 
-		queue.offer(new Point(starti, startj));
-
-		while (!queue.isEmpty()) {
-			Point cur = queue.poll();
-			int size = list.size();
-
-			PriorityQueue<Point> pq = new PriorityQueue<>(new Comparator<Point>() {
-
-				@Override
-				public int compare(Point o1, Point o2) {
-					if (o1.dis == o2.dis) {
-						if (o1.i == o2.i)
-							return o1.j - o2.j;
-						return o1.i - o2.i;
-					}
-					return o1.dis - o2.dis;
-				}
-			});
-
-			for (int i = 0; i < size; i++) {
-				Point fish = list.get(i);
-
-				if (fish.weight >= baby)
-					break;
-
-				int dis = getDistance(cur, fish);
-
-				if (dis != -1)
-					pq.offer(new Point(fish.i, fish.j, dis, i));
-
-			}
-
-			if (pq.isEmpty())
-				return;
-
-			Point eatenFish = pq.poll();
-
-			// 잡아먹힌 물고기 제거 & 아기 상어 이동
-			arr[eatenFish.i][eatenFish.j] = 0;
-			list.remove(eatenFish.listIdx);
-			time += eatenFish.dis;
-			queue.offer(new Point(eatenFish.i, eatenFish.j));
-
-			// 아기 상어 성장
-			if (baby == ++cnt) {
-				baby++;
-				cnt = 0;
-			}
-
-		} // end while
-
-	}
-
-	private static int getDistance(Point babyShark, Point fish) {
-		Queue<Point> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.weight, o2.weight));
 		boolean[][] visit = new boolean[N][N];
 
-		pq.offer(new Point(babyShark.i, babyShark.j, 0));
-		visit[babyShark.i][babyShark.j] = true;
-
-		int result = -1;
+		visit[sharkI][sharkJ] = true;
+		pq.offer(new Point(sharkI, sharkJ, 0));
 
 		while (!pq.isEmpty()) {
 			Point cur = pq.poll();
 
-			if (cur.i == fish.i && cur.j == fish.j) {
-				return result = cur.weight;
+			if (arr[cur.i][cur.j] > 0 && arr[cur.i][cur.j] < baby) {
+
+				time = cur.dis;
+				arr[cur.i][cur.j] = 0;
+
+				if (baby == ++cnt) {
+					baby++;
+					cnt = 0;
+					if (--fishCnt == 0)
+						return;
+				}
+
+				visit = new boolean[N][N];
+				visit[cur.i][cur.j] = true;
+				pq.clear();
 			}
+
 			for (int d = 0; d < 4; d++) {
 				int nexti = cur.i + di[d];
 				int nextj = cur.j + dj[d];
@@ -138,33 +90,30 @@ public class Main {
 					continue;
 
 				visit[nexti][nextj] = true;
-				pq.offer(new Point(nexti, nextj, cur.weight + 1));
-
+				pq.offer(new Point(nexti, nextj, cur.dis + 1));
 			}
-		}
 
-		return result;
+		} // end while
+
 	}
 
-	static class Point {
-		int i, j, weight, dis, listIdx;
+	static class Point implements Comparable<Point> {
+		int i, j, dis;
 
-		public Point(int i, int j) {
-			this.i = i;
-			this.j = j;
-		}
-
-		public Point(int i, int j, int weight) {
-			this.i = i;
-			this.j = j;
-			this.weight = weight;
-		}
-
-		public Point(int i, int j, int dis, int listIdx) {
+		public Point(int i, int j, int dis) {
 			this.i = i;
 			this.j = j;
 			this.dis = dis;
-			this.listIdx = listIdx;
+		}
+
+		@Override
+		public int compareTo(Point o) {
+			if (this.dis == o.dis) {
+				if (this.i == o.i)
+					return this.j - o.j;
+				return this.i - o.i;
+			}
+			return this.dis - o.dis;
 		}
 	}
 
