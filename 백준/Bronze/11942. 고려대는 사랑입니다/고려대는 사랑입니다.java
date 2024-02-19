6b4f102527235b0672e2049aee2cf2d97b85ac58
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-02-19 23:58:21 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 120 ms, Memory: 13844 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		System.out.println("고려대학교");
+
+	}
+
+}
