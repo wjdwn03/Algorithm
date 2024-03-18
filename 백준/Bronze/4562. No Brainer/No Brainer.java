919fff5954ed35d0740c6f7c8bceaae923636f8e
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-03-18 23:44:12 +0900

    [Bronze IV] Title: No Brainer, Time: 116 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/4562. No Brainer/No Brainer.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a < b)
+				sb.append("NO BRAINS");
+			else
+				sb.append("MMM BRAINS");
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
