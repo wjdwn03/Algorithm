7e029a246c582e508bb61de17cd21219a2cb87bb
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-03-22 23:55:16 +0900

    [Bronze IV] Title: 모음의 개수, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String input = br.readLine();
+
+		int cnt = 0;
+
+		Set<Character> set = new HashSet<>();
+		set.add('a');
+		set.add('e');
+		set.add('i');
+		set.add('o');
+		set.add('u');
+
+		for (int i = 0; i < input.length(); i++) {
+			if (set.contains(input.charAt(i)))
+				cnt++;
+		}
+
+		System.out.println(cnt);
+	}
+
+}
