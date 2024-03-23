2a6ac2cd114614329783dfc2f6ebd255a5cf0724
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-03-23 23:00:28 +0900

    [Silver III] Title: 배열 복원하기, Time: 504 ms, Memory: 41280 KB -BaekjoonHub

---
+++ 백준/Silver/16967. 배열 복원하기/배열 복원하기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+
+		int H = Integer.parseInt(st.nextToken());
+		int W = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		int Y = Integer.parseInt(st.nextToken());
+
+		int[][] B = new int[H + X][W + Y];
+		int[][] A = new int[H][W];
+
+		for (int i = 0; i < H + X; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < W + Y; j++) {
+				B[i][j] = Integer.parseInt(st.nextToken());
+			}
+		} // end input
+
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				if (i < X || j < Y) {
+					A[i][j] = B[i][j];
+				} else {
+					A[i][j] = B[i][j] - A[i - X][j - Y];
+				}
+			}
+		}
+
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				sb.append(A[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
