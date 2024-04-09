57a638b6db03199f27b5855df90a622348793ac8
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-04-09 12:52:47 +0900

    [Bronze IV] Title: Hello Judge, Time: 116 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i=1; i<=N; i++){
+            sb.append("Hello World, Judge ").append(i).append("!").append("\n");
+        }
+        System.out.println(sb.toString());
+        
+    }
+}
