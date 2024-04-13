392846cbcc3517ba5b66dd0cc9574e7aeaaab166
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-04-13 23:11:35 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 136 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			for (int j = 1; j <= 2 * N - 2 * i; j++) {
+				sb.append(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				sb.append("*");
+			}
+			for (int j = 1; j <= 2 * i; j++) {
+				sb.append(" ");
+			}
+			for (int j = 1; j <= N - i; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
