3444bcbf753fe09892d13d7e097cf6b0959d49d9
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-04-17 10:14:14 +0900

    [Bronze V] Title: We love kriii, Time: 120 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+        sb.append("강한친구 대한육군").append("\n");
+        sb.append("강한친구 대한육군").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
