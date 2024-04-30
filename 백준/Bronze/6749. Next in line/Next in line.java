4c5740b1f17aafc6140cde7f034b0eb8cc56d681
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-04-30 23:51:38 +0900

    [Bronze IV] Title: Next in line, Time: 120 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int y = Integer.parseInt(br.readLine());
+		int m = Integer.parseInt(br.readLine());
+
+		System.out.println(m + (m - y));
+	}
+
+}
