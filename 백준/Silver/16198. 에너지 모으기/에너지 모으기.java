d20507c85fccc8f07a82e9f320c9cdebc8a0ae7d
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-05-06 16:30:47 +0900

    [Silver I] Title: 에너지 모으기, Time: 144 ms, Memory: 18156 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.java
@@ -2,19 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
-class Marble {
-	int[] arr;
-	int size, energy;
-
-	public Marble(int[] arr, int size, int energy) {
-		this.arr = arr;
-		this.size = size;
-		this.energy = energy;
-	}
-}
 
 public class Main {
 
@@ -33,15 +23,6 @@ public class Main {
 			origin[i] = Integer.parseInt(st.nextToken());
 		} // end input
 
-		bfs();
-
-		System.out.println(max);
-	}
-
-	private static void bfs() {
-
-		Queue<Marble> queue = new ArrayDeque<>();
-
 		for (int i = 1; i < N - 1; i++) {
 			int[] arr = new int[N - 1];
 			int idx = 0;
@@ -52,31 +33,31 @@ public class Main {
 				arr[idx++] = origin[j];
 			}
 
-			queue.offer(new Marble(arr, N - 1, origin[i - 1] * origin[i + 1]));
+			dfs(arr, N - 1, origin[i - 1] * origin[i + 1]);
 		}
 
-		while (!queue.isEmpty()) {
-			Marble cur = queue.poll();
+		System.out.println(max);
+	}
 
-			if (cur.size == 2) {
-				max = Math.max(max, cur.energy);
-				continue;
-			}
+	private static void dfs(int[] arr, int size, int energy) {
 
-			for (int i = 1; i < cur.size - 1; i++) {
-				int[] newArr = new int[cur.size - 1];
-				int idx = 0;
-
-				for (int j = 0; j < cur.size; j++) {
-					if (i == j)
-						continue;
-					newArr[idx++] = cur.arr[j];
-				}
-
-				queue.offer(new Marble(newArr, cur.size - 1, cur.energy + cur.arr[i - 1] * cur.arr[i + 1]));
-			}
+		if (size == 2) {
+			max = Math.max(max, energy);
+			return;
 		}
 
+		for (int i = 1; i < size - 1; i++) {
+			int[] newArr = new int[size - 1];
+			int idx = 0;
+
+			for (int j = 0; j < size; j++) {
+				if (i == j)
+					continue;
+				newArr[idx++] = arr[j];
+			}
+
+			dfs(newArr, size - 1, energy + arr[i - 1] * arr[i + 1]);
+		}
 	}
 
 }
