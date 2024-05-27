6b36f0fe1b15733ad16091d4d982d80de74468ab
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-05-27 22:37:34 +0900

    [Bronze V] Title: The World Responds, Time: 108 ms, Memory: 13952 KB -BaekjoonHub

--- 백준/Bronze/23234. The World Responds/The World Responds.java
+++ 백준/Bronze/23234. The World Responds/The World Responds.java
@@ -1,13 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
+	public static void main(String[] args) throws IOException {
+
 		System.out.println("The world says hello!");
 	}
-
-	
 
 }
