f3887a74690ecfc86e04da82c961011efd10b39d
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-05-27 22:38:32 +0900

    [Bronze V] Title: 나는 누구인가, Time: 120 ms, Memory: 13920 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		System.out.println("I'm Sexy");
+	}
+
+}
