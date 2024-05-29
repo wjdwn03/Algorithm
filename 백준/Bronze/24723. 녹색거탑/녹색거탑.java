c3274f1b0d54fbc4cf3aa45283fa5f2426448ff2
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-05-29 23:58:55 +0900

    [Bronze IV] Title: 녹색거탑, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.Math;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        System.out.println((int)Math.pow(2, N));
+
+	}
+
+}
