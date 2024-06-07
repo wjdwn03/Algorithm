915267ade8b2adc2672958b058fb678fceecb07f
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-06-07 23:58:37 +0900

    [Bronze V] Title: 라면 공식, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+
+			sb.append(a * (x - 1) + b).append("\n");
+		}
+
+		System.out.println(sb.toString());
+
+	}
+
+}
