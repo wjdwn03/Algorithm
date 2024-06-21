e0062be6bf382eedbd3aec201ee5ac54fbfc33a4
Jeongju Hwang <109848753+wjdwn03@users.noreply.github.com>
2024-06-22 01:52:32 +0900

    [Bronze V] Title: NFC West vs North, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

---
+++ 백준/Bronze/10170. NFC West vs North/NFC West vs North.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append("NFC West       W   L  T").append("\n");
+		sb.append("-----------------------").append("\n");
+		sb.append("Seattle        13  3  0").append("\n");
+		sb.append("San Francisco  12  4  0").append("\n");
+		sb.append("Arizona        10  6  0").append("\n");
+		sb.append("St. Louis      7   9  0").append("\n").append("\n");
+		sb.append("NFC North      W   L  T").append("\n");
+		sb.append("-----------------------").append("\n");
+		sb.append("Green Bay      8   7  1").append("\n");
+		sb.append("Chicago        8   8  0").append("\n");
+		sb.append("Detroit        7   9  0").append("\n");
+		sb.append("Minnesota      5  10  1").append("\n");
+
+		System.out.println(sb.toString());
+	}
+
+}
